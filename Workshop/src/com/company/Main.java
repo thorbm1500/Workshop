166f128ccf7de941c7b7851c6fca100f8442aa24
thorbm1500 <43374833+thorbm1500@users.noreply.github.com>
2021-09-06 10:31:36 +0200

    Update Main.java
    Tilføjet kommentare

--- Workshop/src/com/company/Main.java
+++ Workshop/src/com/company/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Skriv dit input");
+        //Printer teksten
         Scanner a = new Scanner(System.in);
+        //Gør det muligt at modtage input
 
         int Input = a.nextInt();
+        //Modtager input
         System.out.println(Input);
+        //Printer dit første input
         int Input2 = a.nextInt();
+        //Modtager dit andet input
 
         System.out.println(Input+Input2);
+        //Ligger dine input sammen og printer det
     }
 }
